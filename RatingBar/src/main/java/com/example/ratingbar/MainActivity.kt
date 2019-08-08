47d75f45bdbf00c13c1d16dda22e573b76d12cfc
visalrotanak <45762799+visalrotanak@users.noreply.github.com>
2019-08-08 19:19:31 +0700

    Add files via upload

---
+++ RatingBar/src/main/java/com/example/ratingbar/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.ratingbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        ratingBar.setOnRatingBarChangeListener { ratingBar, fl, b ->
+            when(ratingBar.id) {
+                R.id.ratingBar -> {
+                    textView2.text = fl.toString()
+                }
+            }
+        }
+    }
+
+}
